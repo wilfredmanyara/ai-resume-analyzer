7bdfb2ccff39901760faa28cd47886d226fa99a2
Wilfred Manyara <manyarawilly46@gmail.com>
2025-07-23 15:50:17 +0300

    feat(ai-resume-analyzer): cloud storage, PDF-Image, AI feedback

--- app/routes.ts
+++ app/routes.ts
@@ -2,7 +2,8 @@ import {index, route, type RouteConfig} from "@react-router/dev/routes";
 
 export default [
     index("routes/home.tsx"),
-    route('/auth', 'routes/auth.tsx')
+    route('/auth', 'routes/auth.tsx'),
+    route('/upload', 'routes/upload.tsx')
 
 ] satisfies RouteConfig;
 
