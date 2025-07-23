0daa6fc2ad9a0aad21691c1cc85df0e8965fae04
Wilfred Manyara <manyarawilly46@gmail.com>
2025-07-23 17:00:24 +0300

    feat(ai-resume-analyzer): resume feedback

--- app/routes.ts
+++ app/routes.ts
@@ -3,7 +3,8 @@ import {index, route, type RouteConfig} from "@react-router/dev/routes";
 export default [
     index("routes/home.tsx"),
     route('/auth', 'routes/auth.tsx'),
-    route('/upload', 'routes/upload.tsx')
+    route('/upload', 'routes/upload.tsx'),
+    route('/resume/:id', 'routes/resume.tsx')
 
 ] satisfies RouteConfig;
 
