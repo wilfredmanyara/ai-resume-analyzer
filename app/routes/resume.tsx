ea9ce1a74239d333de39bd6ca885e7e85be4bfa7
Wilfred Manyara <72160527+wilfredmanyara@users.noreply.github.com>
2025-07-29 18:31:15 +0300

    fix(ai-resume-analyzer): update resume

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -6,7 +6,7 @@ import ATS from "~/components/ATS";
 import Details from "~/components/Details";
 
 export const meta = () => ([
-    {title: 'Resumind | Review '},
+    {title: 'Resulyze | Review '},
     {name: 'description', content: 'Detailed overview of your resume'},
 ])
 
